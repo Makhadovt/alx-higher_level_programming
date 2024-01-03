9b24d78c638363fa45e1a5d9cdcd9a289ef56953
Makhadovt <makhadovhutalitracey@gmail.com>
2024-01-03 04:29:41 -0800

    Run Python file

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -7,22 +7,21 @@
  * Return: 1 if the list has a cycle, 0 if it doesn't
  */
 int check_cycle(listint_t *list)
+{listint_t *slow = list;
+listint_t *fast = list;
+
+if (!list)
+return (0);
+
+while (slow && fast && fast->next)
 {
-        listint_t *slow = list;
-        listint_t *fast = list;
+slow = slow->next;
+fast = fast->next->next;
+if (slow == fast)
+return (1);
+}
 
-        if (!list)
-                return (0);
-
-        while (slow && fast && fast->next)
-        {
-                slow = slow->next;
-                fast = fast->next->next;
-                if (slow == fast)
-                        return (1);
-        }
-
-        return (0);
+return (0);
 }
 root@e024474929d2:~/alx-higher_level_programming/0x00-python-hello_world# cat lists.h
 #ifndef LISTS_H
