8ca45415af80e10198ad263ad9cfd285afbbb4d4
Makhadovt <makhadovhutalitracey@gmail.com>
2024-01-03 04:49:20 -0800

    task

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -7,7 +7,8 @@
  * Return: 1 if the list has a cycle, 0 if it doesn't
  */
 int check_cycle(listint_t *list)
-{listint_t *slow = list;
+{
+listint_t *slow = list;
 listint_t *fast = list;
 
 if (!list)
@@ -28,3 +29,26 @@ root@e024474929d2:~/alx-higher_level_programming/0x00-python-hello_world# cat li
 #define LISTS_H
 
 #include <stdlib.h>
+
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+typedef struct listint_s
+{
+int n;
+struct listint_s *next;
+} listint_t;
+
+size_t print_listint(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
+void free_listint(listint_t *head);
+int check_cycle(listint_t *list);
+
+#endif /* LISTS_H */
+
+
